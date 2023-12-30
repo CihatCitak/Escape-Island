346e0d5ebbc191e6e30a5285b489c2c4e4fe71bc
Cihat Çıtak <cihatcitakc@gmail.com>
2023-12-30 22:19:48 +0300

    Refactor level data can't hold prefabs any more

--- Assets/Scripts/LevelDataSystem/LevelData.cs
+++ Assets/Scripts/LevelDataSystem/LevelData.cs
@@ -7,11 +7,6 @@ namespace LevelDataSystem
     [CreateAssetMenu(menuName = "Level Data/Level", fileName = "Level")]
     public class LevelData : ScriptableObject
     {
-        [Header("Prefabs")]
-        public FieldViewer FieldPrefab;
-        public GameObject PawnPrefab;
-        public GameObject ClosePawnPrefab;
-        [Header("Field Datas")]
         public List<FieldModelScriptable> FieldModels;
     }
 }
