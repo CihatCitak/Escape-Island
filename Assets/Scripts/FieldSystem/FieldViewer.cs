9742b4ff871491dc42a1ead37c0fc5ae37cca0a9
Cihat Çıtak <cihatcitakc@gmail.com>
2023-12-30 22:24:57 +0300

    Refactor FieldViewer  tween logic

--- Assets/Scripts/FieldSystem/FieldViewer.cs
+++ Assets/Scripts/FieldSystem/FieldViewer.cs
@@ -6,24 +6,34 @@ namespace FieldSystem
 {
     public class FieldViewer : MonoBehaviour, IClickable
     {
-        public bool IsClickable { get; set; }
+        [SerializeField] private Transform pawnPositionParent;
+        private Tween moveTween;
 
+        public bool IsClickable { get; set; }
         public void OnCLick()
         {
-            // Yukarı kaldır
-            transform.DOMoveY(1f, 1f).SetEase(Ease.InOutSine).Play();
+            if (moveTween != null)
+                moveTween.Kill();
+
+            moveTween = transform.DOMoveY(1f, 1f).SetEase(Ease.InOutSine).Play();
         }
 
         public void OnClose()
         {
-            // Bayrak çıkart
             IsClickable = false;
         }
 
         public void ResetClick()
         {
-            // Aşağıya indir
-            transform.DOMoveY(0f, 0.5f).SetEase(Ease.InOutSine).Play();
+            if (moveTween != null)
+                moveTween.Kill();
+
+            moveTween = transform.DOMoveY(0f, 0.5f).SetEase(Ease.InOutSine).Play();
+        }
+
+        public Transform GetPawnPositionParent()
+        {
+            return pawnPositionParent;
         }
     }
 }
