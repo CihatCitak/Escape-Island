36eadb691b133046c216a82c8ff5a27673d759ab
Cihat Çıtak <cihatcitakc@gmail.com>
2023-12-31 21:30:40 +0300

    Add MainUI basic elements

---
+++ Assets/Scripts/UISystem/MainUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace UISystem
+{
+    public class MainUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject GameUI;
+        [SerializeField] private GameObject LevelEndUI;
+    }
+}
